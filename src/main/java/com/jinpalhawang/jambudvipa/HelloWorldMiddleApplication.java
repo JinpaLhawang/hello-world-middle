01702160b465768cece54a0b84d6463c67bfb255
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-04-27 10:18:18 -0400

    Added config client support. Reads property from config server and logs.

--- src/main/java/com/jinpalhawang/jambudvipa/HelloWorldMiddleApplication.java
+++ src/main/java/com/jinpalhawang/jambudvipa/HelloWorldMiddleApplication.java
@@ -7,11 +7,14 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +22,30 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
 public class HelloWorldMiddleApplication {
+
+  @Autowired
+  void setEnvironment(Environment e) {
+    System.out.println(e.getProperty("helloWorld"));
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloWorldMiddleApplication.class, args);
+  }
+
+}
+
+@RestController
+@RefreshScope
+class FooRestController {
 
   private static final Logger log = LoggerFactory.getLogger(HelloWorldMiddleApplication.class);
 
   @Autowired
   private DiscoveryClient discoveryClient;
+
+  @Value("${helloWorld}")
+  private String helloWorld;
 
   @RequestMapping("/")
   public String hello() {
@@ -33,6 +53,7 @@ public class HelloWorldMiddleApplication {
     URI uri = URI.create("http://localhost:8090/applications/search/findByName?name=hello-world-middle");
     Application application = restTemplate.getForObject(uri, Application.class);
     log.info(application.toString());
+    log.info(helloWorld);
     return application.getProperty();
   }
 
@@ -48,10 +69,6 @@ public class HelloWorldMiddleApplication {
   @RequestMapping("/service-instances/{applicationName}")
   public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
     return this.discoveryClient.getInstances(applicationName);
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(HelloWorldMiddleApplication.class, args);
   }
 
 }
