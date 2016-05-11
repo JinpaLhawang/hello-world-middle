f67b2593b7f60b6d7e8e7e836be57b6e510a7bbc
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2016-05-11 16:27:05 -0400

    Reverted logging changes. Moved rest controller to own file/

--- src/main/java/com/jinpalhawang/jambudvipa/HelloWorldMiddleApplication.java
+++ src/main/java/com/jinpalhawang/jambudvipa/HelloWorldMiddleApplication.java
@@ -1,64 +1,15 @@
 package com.jinpalhawang.jambudvipa;
 
-import java.net.URI;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class HelloWorldMiddleApplication {
 
-  private static final Logger log = LoggerFactory.getLogger(HelloWorldMiddleApplication.class);
-
   public static void main(String[] args) {
-    final SpringApplication app = new SpringApplication(HelloWorldMiddleApplication.class);
-    app.setLogStartupInfo(false);
-    app.run(args);
-    log.info(HelloWorldMiddleApplication.class.getSimpleName() + " started.");
-  }
-
-}
-
-@RestController
-@RefreshScope
-class FooRestController {
-
-  private static final Logger log = LoggerFactory.getLogger(HelloWorldMiddleApplication.class);
-
-  @Autowired
-  private DiscoveryClient discoveryClient;
-
-  @Value("${helloWorld}")
-  private String helloWorld;
-
-  @RequestMapping("/")
-  public String helloWorld() {
-    RestTemplate restTemplate = new RestTemplate();
-    URI uri = URI.create("http://localhost:8090/accounts/search/findByFirstName?firstName=Jinpa");
-    Account account = restTemplate.getForObject(uri, Account.class);
-    log.info(account.toString());
-    String response = helloWorld.replace("World", account.getFirstName() + " " + account.getLastName());
-    log.info(response);
-    return response;
-  }
-
-  @RequestMapping("/service-instances/{applicationName}")
-  public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
-    return this.discoveryClient.getInstances(applicationName);
+    SpringApplication.run(HelloWorldMiddleApplication.class, args);
   }
 
 }
